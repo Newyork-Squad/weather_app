d6ba19f37b4f7745d1bc1c61b5177bbec7c8d780
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-10-20 23:38:21 +0300

    feat: safe area & gradient background

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/ui/designSystem/theme/AppThemeProvider.dart';
 import 'package:weather_app/ui/designSystem/theme/weather_theme.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:weather_app/presentation/widget/current_weather.dart';
+import 'package:weather_app/ui/screen/home_screen.dart';
 
 void main() {
-  runApp(
-    AppThemeProvider(
-      brightness: Brightness.light,
-      child: const MyApp(),
-    ),
-  );
+  runApp(AppThemeProvider(brightness: Brightness.light, child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -19,60 +13,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = MyWeatherTheme.of(context);
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            CurrentWeather(
-              temperature: 22.0,
-              weatherIcon: 2,
-              weatherMessage: "weatherMessage",
-              maxTemp: 25.0,
-              minTemp: 18.0,
-            ),
-          ],
+    return Directionality(
+      textDirection: TextDirection.ltr,
+      child: Container( // Same background for the entire app
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              theme.colors.backgroundPrimary,
+              theme.colors.backgroundSecondary,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: SafeArea(child: MyHomePage()), // SafeArea to avoid overlaps with system UI
       ),
     );
   }
