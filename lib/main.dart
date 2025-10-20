e5a67ee454a979307eb285078adbc2716f9cfb67
MuhammedWael9991 <mw26200400@gmail.com>
2025-10-20 17:37:59 +0300

    integrate AppThemeProvider and update main app structure for new design system

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/ui/designSystem/theme/AppThemeProvider.dart';
+import 'package:weather_app/ui/designSystem/theme/weather_theme.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    AppThemeProvider(
+      brightness: Brightness.light,
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -9,56 +16,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+    final theme = MyWeatherTheme.of(context);
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+    return Directionality(
+      textDirection: TextDirection.ltr,
+      child: Container(
+        color: theme.colors.backgroundPrimary,
+        child: Center(
+          child: Container(
+            color: theme.colors.surfaceSecondary,
+            padding: const EdgeInsets.all(16),
+            child: Text(
+              "This is the new design system",
+              style: theme.typography.textTheme.headlineLarge?.copyWith(
+                color: theme.colors.shadePrimary
+              ),
             ),
-          ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
