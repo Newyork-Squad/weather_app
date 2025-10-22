08004e2bb5f18a3abd9af022ea38b275d2c7feda
Tasneem AbdElhakeem <tasniim.hakiim@gmail.com>
2025-10-23 00:44:10 +0300

    fix: remove yellow underline text

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,15 @@ import 'package:weather_app/ui/designSystem/theme/weather_theme.dart';
 import 'package:weather_app/ui/screen/home_screen.dart';
 
 void main() {
-  runApp(MaterialApp( debugShowCheckedModeBanner: false, home: AppThemeProvider(brightness: Brightness.light, child: const MyApp())));
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AppThemeProvider(
+        brightness: Brightness.light,
+        child: const MyApp(),
+      ),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -13,20 +21,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = MyWeatherTheme.of(context);
-    return Directionality(
-      textDirection: TextDirection.ltr,
-      child: Container( // Same background for the entire app
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              theme.colors.backgroundPrimary,
-              theme.colors.backgroundSecondary,
-            ],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
+    return Material(
+      child: Directionality(
+        textDirection: TextDirection.ltr,
+        child: Container(
+          // Same background for the entire app
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                theme.colors.backgroundPrimary,
+                theme.colors.backgroundSecondary,
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
           ),
+          child: SafeArea(
+            child: MyHomePage(),
+          ), // SafeArea to avoid overlaps with system UI
         ),
-        child: SafeArea(child: MyHomePage()), // SafeArea to avoid overlaps with system UI
       ),
     );
   }
