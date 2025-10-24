06097932e585d117f7eaf9f08cb9a7f1655ed0e3
abdelrahman <abdelrahmankarim7@gmail.com>
2025-10-24 15:40:48 +0300

    feat :add weather code mapper util

--- lib/utils/weather_utils.dart
+++ lib/utils/weather_utils.dart
@@ -1,6 +1,3 @@
-
-
-
 String getWeatherIconRes(int weatherCode,{bool isDay = true}  ) {
   switch (weatherCode) {
     case 0:
@@ -117,5 +114,68 @@ String getWeatherIconRes(int weatherCode,{bool isDay = true}  ) {
           : 'assets/images/thunderstrom_with_heavy_hail_night.png';
     default:
       return 'assets/images/ic_launcher_background.png';
+  }}
+
+  String mapWeatherCodeToDescription(int? code) {
+    switch (code) {
+      case 0:
+        return "Clear sky";
+      case 1:
+        return "Mainly clear";
+      case 2:
+        return "Partly cloudy";
+      case 3:
+        return "Overcast";
+      case 45:
+        return "Fog";
+      case 48:
+        return "Depositing rime fog";
+      case 51:
+        return "Drizzle: Light intensity";
+      case 53:
+        return "Drizzle: Moderate intensity";
+      case 55:
+        return "Drizzle: Dense intensity";
+      case 56:
+        return "Freezing Drizzle: Light intensity";
+      case 57:
+        return "Freezing Drizzle: Dense intensity";
+      case 61:
+        return "Rain: Slight";
+      case 63:
+        return "Rain: Moderate";
+      case 65:
+        return "Rain: Heavy intensity";
+      case 66:
+        return "Freezing Rain: Light intensity";
+      case 67:
+        return "Freezing Rain: Heavy intensity";
+      case 71:
+        return "Snow fall: Slight intensity";
+      case 73:
+        return "Snow fall: Moderate intensity";
+      case 75:
+        return "Snow fall: Heavy intensity";
+      case 77:
+        return "Snow grains";
+      case 80:
+        return "Rain showers: Slight";
+      case 81:
+        return "Rain showers: Moderate";
+      case 82:
+        return "Rain showers: Violent";
+      case 85:
+        return "Snow showers: Slight";
+      case 86:
+        return "Snow showers: Heavy";
+      case 95:
+        return "Thunderstorm: Slight or moderate";
+      case 96:
+        return "Thunderstorm with slight hail";
+      case 99:
+        return "Thunderstorm with heavy hail";
+      default:
+        return "Unknown";
+    }
   }
-}
+
