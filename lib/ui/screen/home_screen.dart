81c04e8a99e30d5d55c45c8231689f2ccc8a864d
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-10-24 17:24:08 +0300

    refactor: move background to home

--- lib/ui/screen/home_screen.dart
+++ lib/ui/screen/home_screen.dart
@@ -28,66 +28,107 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final theme = MyWeatherTheme.of(context);
     return BlocBuilder<WeatherCubit, WeatherState>(
       builder: (context, state) {
         if (state is WeatherLoading) {
           return const Center(child: CircularProgressIndicator());
         } else if (state is WeatherLoaded) {
           final weather = state.weather;
-          return CustomScrollView(
-            slivers: <Widget>[
-              SliverToBoxAdapter(
-                child: LocationDisplayComponent(
-                  locationName: weather.timezone ?? "Unknown",
-                ),
-              ),
-              CurrentWeather(
-                temperature: weather.current?.temperature2m ?? 0,
-                weatherIcon: getWeatherIconRes(
-                  weather.current?.weatherCode ?? 1,
-                ),
-                weatherMessage: mapWeatherCodeToDescription(weather.current?.weatherCode),
-                maxTemp: weather.daily.isNotEmpty ? weather.daily[0]
-                    .maxTemp : 0,
-                minTemp: weather.daily.isNotEmpty ? weather.daily[0]
-                    .minTemp : 0,
-              ),
-              SliverToBoxAdapter(
-                child: Padding(
-                  padding: const EdgeInsets.only(
-                      left: 12, right: 12, top: 0, bottom: 24),
-                  child: WeatherInfoGrid(
-                    windSpeed: "${weather.current?.windSpeed10m ?? ''}",
-                    humidity: "${weather.current?.relativeHumidity2m ?? ''}",
-                    uvIndex: "${weather.daily[0].uvIndex}",
-                    pressure: "${weather.current?.surfacePressure ?? ''}",
-                    feelsLike: "${weather.current?.apparentTemperature ?? ''}",
-                    rainChance: "${weather.current?.precipitationProbability ?? ''}",
+          return Material(
+            child: Directionality(
+              textDirection: TextDirection.ltr,
+              child: Container(
+                // Same background for the entire app
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      theme.colors.backgroundPrimary,
+                      theme.colors.backgroundSecondary,
+                    ],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
                   ),
                 ),
+                child: SafeArea(
+                  child: CustomScrollView(
+                    slivers: <Widget>[
+                      SliverToBoxAdapter(
+                        child: LocationDisplayComponent(
+                          locationName: weather.timezone ?? "Unknown",
+                        ),
+                      ),
+                      CurrentWeather(
+                        temperature: weather.current?.temperature2m ?? 0,
+                        weatherIcon: getWeatherIconRes(
+                          weather.current?.weatherCode ?? 1,
+                        ),
+                        weatherMessage: mapWeatherCodeToDescription(
+                          weather.current?.weatherCode,
+                        ),
+                        maxTemp: weather.daily.isNotEmpty
+                            ? weather.daily[0].maxTemp
+                            : 0,
+                        minTemp: weather.daily.isNotEmpty
+                            ? weather.daily[0].minTemp
+                            : 0,
+                      ),
+                      SliverToBoxAdapter(
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                            left: 12,
+                            right: 12,
+                            top: 0,
+                            bottom: 24,
+                          ),
+                          child: WeatherInfoGrid(
+                            windSpeed: "${weather.current?.windSpeed10m ?? ''}",
+                            humidity:
+                                "${weather.current?.relativeHumidity2m ?? ''}",
+                            uvIndex: "${weather.daily[0].uvIndex}",
+                            pressure:
+                                "${weather.current?.surfacePressure ?? ''}",
+                            feelsLike:
+                                "${weather.current?.apparentTemperature ?? ''}",
+                            rainChance:
+                                "${weather.current?.precipitationProbability ?? ''}",
+                          ),
+                        ),
+                      ),
+                      SliverToBoxAdapter(
+                        child: WeatherTodayWidget(
+                          items: weather.hourly
+                              .map(
+                                (hour) => WeatherItem(
+                                  imagePath: getWeatherIconRes(
+                                    hour.weatherCode,
+                                  ),
+                                  temperature: "${hour.temperature}",
+                                  hour: extractHourMinute(hour.time),
+                                ),
+                              )
+                              .toList(),
+                        ),
+                      ),
+                      SliverToBoxAdapter(
+                        child: WeeklyForecastWidget(
+                          weeklyForecast: weather.daily
+                              .map(
+                                (day) => WeatherDayCard(
+                                  day: day.date,
+                                  maxTemp: day.maxTemp.toInt(),
+                                  minTemp: day.minTemp.toInt(),
+                                  imagePath: getWeatherIconRes(day.weatherCode),
+                                ),
+                              )
+                              .toList(),
+                        ),
+                      ),
+                    ],
+                  ),
+                ), // SafeArea to avoid overlaps with system UI
               ),
-              SliverToBoxAdapter(
-                child: WeatherTodayWidget(
-                  items: weather.hourly.map((hour) =>
-                      WeatherItem(
-                        imagePath: getWeatherIconRes(hour.weatherCode),
-                        temperature: "${hour.temperature}",
-                        hour: extractHourMinute(hour.time),
-                      )).toList(),
-                ),
-              ),
-              SliverToBoxAdapter(
-                child: WeeklyForecastWidget(
-                  weeklyForecast: weather.daily.map((day) =>
-                      WeatherDayCard(
-                        day: day.date,
-                        maxTemp: day.maxTemp.toInt(),
-                        minTemp: day.minTemp.toInt(),
-                        imagePath: getWeatherIconRes(day.weatherCode),
-                      )).toList(),
-                ),
-              ),
-            ],
+            ),
           );
         } else if (state is WeatherError) {
           return Center(child: Text('Error: ${state.message}'));
@@ -95,7 +136,5 @@ class _MyHomePageState extends State<MyHomePage> {
         return const SizedBox();
       },
     );
-  }}
-
-
-
+  }
+}
